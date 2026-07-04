cf53aed86e348007facf75c02c78e44ead40bf06
Sarvesh Avhad <sarveshavhad4@gmail.com>
2026-07-04 14:21:39 +0530

    Whatsapp channel link added

--- apps/web/src/config/site.ts
+++ apps/web/src/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   socials: {
-    whatsappCommunity: 'https://chat.whatsapp.com/example-invite-link',
+    whatsappCommunity: 'https://whatsapp.com/channel/0029VaGIajp9hXF49C4G4N3J',
     instagram: 'https://instagram.com/itsa_dypdpu',
     linkedin: 'https://www.linkedin.com/company/itsa-dpu',
     facebook: 'https://facebook.com/itsa.dpu'
